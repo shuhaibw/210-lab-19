742344314c1007cc601a9df892793510ad40cc02
shuhaibw <120868621+shuhaibw@users.noreply.github.com>
2024-10-06 21:26:18 -0700

    update

--- lab19.cpp
+++ lab19.cpp
@@ -1,1 +1,32 @@
 // COMSC-210 | Lab 19 | Shuhaib Walahi
+
+#include <iostream>
+
+using namespace std;
+
+// struct for movie review nodes
+struct ReviewNode
+{
+    float rating;
+    string comments;
+    ReviewNode* next;
+};
+
+// class for movie
+class Movie
+{
+    private:
+        string title;
+        // pointer to head of reviews linked list
+        ReviewNode* reviewsHead; 
+    public:
+        // constructor to initialize a movie object with a title
+        Movie(const string& t) : title(t), reviewsHead(nullptr) {}
+        // destructor
+        
+};
+
+int main
+{
+    return 0;
+}
